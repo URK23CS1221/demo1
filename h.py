84fe2b925eb8bcbaa7491227ed971a65ccf178aa
dharshan power <dhancharladharshan@karunya.edu.in>
2024-02-26 09:24:29 +0530

    fix#1 resolved the isssue

--- h.py
+++ h.py
@@ -1,1 +1,19 @@
-print("ntg")
+inven={"gold":500,
+      
+      'pouch':['hint','twinw','gemstinne'],
+    'backpack':['xylo','dragger','bedleaf','breadroll'] ,
+
+      
+      }
+
+inven['pouch']=['seashell',"strange",'berry']
+inven['backpack'].sort()
+inven['gold']+=50
+
+print(inven)
+
+
+
+
+
+
